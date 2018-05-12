6aa6703d7fa1ab768433539707c407b28f0e0ca0
pikapika <aoxbtksgo13@naver.com>
2018-05-12 11:12:02 +0900

    수정중

--- src/main/java/com/blog/coffee/web/BoardController.java
+++ src/main/java/com/blog/coffee/web/BoardController.java
@@ -2,9 +2,33 @@ package com.blog.coffee.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class BoardController {
+	
+	//게시판 작성페이지 출력
+	@RequestMapping(value="/board/boardInsert", method=RequestMethod.GET)
+	public String boardInset() {
+		
+		return "board/boardInsert";
+	}
+	
+	//게시판 입력 action
+	
+	
+	//게시판 목록페이지 출력
+	
+	
+	//게시판목록 action
+	
+	
+	//게시판 수정
+	
+	
+	//게시판 삭제
+	
+	
 
 }
