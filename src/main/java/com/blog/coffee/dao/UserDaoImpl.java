6ba650442611487ae9e0f5c9a0f2889b974f84f9
pikapika <aoxbtksgo13@naver.com>
2018-05-16 12:28:40 +0900

    수정중

--- src/main/java/com/blog/coffee/dao/UserDaoImpl.java
+++ src/main/java/com/blog/coffee/dao/UserDaoImpl.java
@@ -2,10 +2,12 @@ package com.blog.coffee.dao;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.blog.coffee.User;
 import com.blog.coffee.mappers.UserMapper;
 
+@Repository
 public class UserDaoImpl implements UserDao {
 	
 	@Autowired
